a60760211bd1e67114a2e52dd4706f8d151c7258
bhawkeswood <bhawkeswood@gmail.com>
2013-09-23 17:50:23 -0400

    Completing the variables exercise

--- js_fundamentals/variables.js
+++ js_fundamentals/variables.js
@@ -6,6 +6,17 @@
 
 // Store the following into variables: number of children, partner's name, geographic location, job title.
 // Output your fortune to the screen like so: "You will be a X in Y, and married to Z with N kids."
+var numberOfChildren = 10
+	, partnerName = "Bill"
+	, place = "Toronto"
+	, jobTitle = "Grifter";
+
+function fortuneTeller() {
+	return "You will be a " + jobTitle + " in " + place + ", and married to "
+	 +  partnerName + " with " + numberOfChildren +  " kids.";
+}
+
+fortuneTeller()
 
 // EXERCISE: The Age Calculator
 
@@ -15,6 +26,19 @@
 // Store their birth year in a variable.
 // Calculate their 2 possible ages based on the stored values.
 // Output them to the screen like so: "They are either NN or NN", substituting the values.
+var currentYear = 2013
+	, birthYear = 1982;
+
+var possibleAge1 = (currentYear - birthYear)
+	, possibleAge2 = (currentYear - birthYear - 1);
+
+function ageCalculator() {
+	return "The person in question is either " + possibleAge1
+	 + " or " + possibleAge2 + " years old.";
+}
+
+ageCalculator()
+
 
 // EXERCISE: The Lifetime Supply Calculator
 
@@ -26,6 +50,20 @@
 // Calculate how many you would eat total for the rest of your life.
 // Output the result to the screen like so: "You will need NN to last you until the ripe old age of X".
 
+var currentAge = 31
+	,   maxAge = 81
+	, beersPerDay = 2;
+
+var lifetimeSupplyOfBeer = ( maxAge - currentAge ) * 365 * beersPerDay;
+
+// CURIOUSLY, I WANTED TO KNOW HOW MANY CALORIES THAT WOULD ADD TO MY WAIST, SO
+// 218 kcal per 500ml according to fatsecret.ca
+var beerCalories = 218;
+
+lifetimeSupplyOfBeer;
+lifetimeSupplyOfBeer * beerCalories
+
+
 // EXERCISE: The Geometrizer
 
 // Calculate properties of a circle, using the definitions here.
@@ -33,6 +71,18 @@
 // Store a radius into a variable.
 // Calculate the circumference based on the radius, and output "The circumference is NN".
 // Calculate the area based on the radius, and output "The area is NN".
+var r = 15
+var pi = 3.14
+var d = r*2
+var c = pi*d
+var a = pi*(Math.pow(r,2));
+
+function geometrizer() {
+	return "The circumference is " + c +"m, and the area is " + a + "m square.";
+}
+
+geometrizer()
+
 
 // EXERCISE: The Temperature Converter
 
@@ -42,5 +92,24 @@
 // Convert it to fahrenheit and output "NN°C is NN°F".
 // Now store a fahrenheit temperature into a variable.
 // Convert it to celsius and output "NN°F is NN°C."
+
+var celsius = 34;
+var farenheit = 89;
+var celsiusConverted = celsius * (9/5) + 32;
+var farenheitConverted = (farenheit - 32) * (5/9);
+
+function celsiusConverter() {
+	return  celsius + "°C is " + celsiusConverted + "°F";
+}
+
+function farenheitConverter() {
+	return  farenheit + "°F is " + farenheitConverted + "°C";
+}
+
+celsiusConverter();
+farenheitConverter();
+
+
+
 
 
