bdd4e78faa33e41814d46912db25d4edc8d93615
bhawkeswood <bhawkeswood@gmail.com>
2013-09-24 17:35:46 -0400

    Working through the third of the if else exercises

--- js_fundamentals/ifelse.js
+++ js_fundamentals/ifelse.js
@@ -7,12 +7,42 @@
 // returns whichever number is the greater (higher) number.
 // Call that function 2 times with different number pairs, and log the output to make sure it works (e.g. "The greater number of 5 and 10 is 10.").
 
+
+function greaterNum(num1, num2) {
+	if (num1 > num2){
+		console.log("The greater number of " + num1 + " and " + num2 + 
+			" is " + num1 + ".");
+	}else{
+		console.log("The greater number of " + num1 + " and " + num2 + 
+			" is " + num2 + ".");
+	}
+}
+
+greaterNum(10,25);
+greaterNum(33,2);
 // EXERCISE: The World Translator
 
 // Write a function named helloWorld that:
 // takes 1 argument, a language code (e.g. "es", "de", "en")
 // returns "Hello, World" for the given language, for atleast 3 languages. It should default to returning English.
 // Call that function for each of the supported languages and log the result to make sure it works.
+
+function helloWorld(lang){
+	if (lang == "jp"){
+		console.log("こんにちは世界！");
+	}else if (lang == "de"){
+		console.log("Hallo Welt");
+	}else if (lang == "fr"){
+		console.log("Bonjour, tout le monde!");
+	}else{
+		console.log("Hello, World!");
+	}
+}
+
+helloWorld("jp");
+helloWorld("de");
+helloWorld("fr");
+helloWorld();
 
 // EXERCISE: The Grade Assigner
 
@@ -21,6 +51,25 @@
 // returns a grade for the score, either "A", "B", "C", "D", or "F".
 // Call that function for a few different scores and log the result to make sure it works.
 
+function assignGrade(numScore){
+	if (numScore >= 90){
+		console.log("You got an A!  Congratulations!");
+	}else if (90 > numScore && numScore >= 80){
+		console.log("You got a B.  Not bad.");
+	}else if (80 > numScore && numScore >= 70){
+		console.log("Okay, you got a C.  Welcome to mediocrity.");
+	}else if (70 > numScore && numScore >= 60){
+		console.log("You got a D.  Time to hit the books, eh?");
+	}else {
+		console.log("You got an F. F means fail, dummy.");
+	}
+}
+
+assignGrade(94);
+assignGrade(82);
+assignGrade(76);
+assignGrade(61);
+assignGrade(22);
 // EXERCISE: The Pluralizer
 
 // Write a function named pluralize that:
