8777e5dd781cb062edff96a99fb07ab8239cedf1
bhawkeswood <bhawkeswood@gmail.com>
2013-09-25 16:43:52 -0400

    Frustrated with image swap, moving to jquery

--- js_fundamentals/dom.js
+++ js_fundamentals/dom.js
@@ -10,6 +10,21 @@
 // Find the Google search button and store it in a variable.
 // Modify the text of the button so that it says "Yahooo!" instead.
 
+var googleLogo = new Image();
+googleLogo.src = document.getElementById("hplogo");
+googleLogo.src = "http://l.yimg.com/rz/d/yahoo_frontpage_en-CA_s_f_p_101x50_frontpage.png";
+
+
+// function changeSrc(){
+// 	document.getElementById("hplogo").src = "http://l.yimg.com/rz/d/yahoo_frontpage_en-CA_s_f_p_101x50_frontpage.png";
+// }
+
+// changeSrc();
+
+
+
+
+
 // EXERCISE: About Me
 
 // Cuz every webpage needs an "About" section.
